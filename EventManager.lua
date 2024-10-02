07be122281adaaeab69ec5bbccaafde787ac5aba
SCPFBluesky <144206470+SCPFBluesky@users.noreply.github.com>
2024-10-01 22:58:03 -0500

    Create EventManager.lua

---
+++ EventManager.lua
@@ -0,0 +1,16 @@
+--!strict
+--!native
+-- This Moudle was Written for Atlas-Framework : https://github.com/SCPFBluesky/Atlas-Framework
+local CollectionService = game:GetService("CollectionService")
+local EventManager = {}
+
+function EventManager:BindToTag(Tag, Callback)
+	for _, instance in ipairs(CollectionService:GetTagged(Tag)) do
+		task.spawn(Callback, instance)
+	end
+	return CollectionService:GetInstanceAddedSignal(Tag):Connect(function(instance)
+		task.spawn(Callback, instance)
+	end)
+end
+
+return EventManager
