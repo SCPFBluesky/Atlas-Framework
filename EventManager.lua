fdcd14aab2aceec9c755ecd86e5eb67522dd235f
SCPFBluesky <144206470+SCPFBluesky@users.noreply.github.com>
2024-10-09 23:37:59 -0500

    Update EventManager.lua

--- EventManager.lua
+++ EventManager.lua
@@ -4,13 +4,13 @@
 local CollectionService = game:GetService("CollectionService")
 local EventManager = {}
 
-function EventManager:BindToTag(Tag, Callback)
+@native function EventManager:BindToTag(Tag, Callback)
 	for _, instance in ipairs(CollectionService:GetTagged(Tag)) do
 		task.spawn(Callback, instance)
 	end
 	return CollectionService:GetInstanceAddedSignal(Tag):Connect(function(instance)
 		task.spawn(Callback, instance)
-	end)
-end
+	end);
+end;
 
 return EventManager
